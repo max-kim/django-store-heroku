53a9f1a7e848402e7ea045f91c18b5ecb888f796
kim.b2f <kim.b2f@gmail.com>
2018-09-25 22:36:55 +0300

    development

--- demo_store/views.py
+++ demo_store/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import HttpResponseRedirect
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponse
 from catalog.models import Category
 
 
 def index_view(request):
-    category = Category.objects.order_by('id').first()
-    if category:
-        return HttpResponseRedirect(category.get_absolute_url())
-    else:
-        return HttpResponseRedirect('www.google.com')
-        # return HttpResponseNotFound('404 - PAGE NOT FOUND :(')
+    return HttpResponse('Hello world')
+    # category = Category.objects.order_by('id').first()
+    # if category:
+    #     return HttpResponseRedirect(category.get_absolute_url())
+    # else:
+    #     return HttpResponseRedirect('www.google.com')
+    #     # return HttpResponseNotFound('404 - PAGE NOT FOUND :(')
